8d824f2bfc789c042e56d0784112b6fe370d774b
sebasGZA <sebassga34@gmail.com>
2025-01-15 18:48:09 -0500

    fix: rename message patthers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { MessagePattern } from '@nestjs/microservices';
+
+import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
@@ -11,12 +12,12 @@ export class AuthController {
     return 'register user';
   }
 
-  @MessagePattern('auth.register.user')
+  @MessagePattern('auth.login.user')
   loginUser() {
     return 'login user';
   }
 
-  @MessagePattern('auth.register.user')
+  @MessagePattern('auth.verify.user')
   verifyToken() {
     return 'verify token ';
   }
