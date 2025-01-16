aa2c1f949b9b9f935774d0f5fa1b8a0da7cdf1f8
sebasGZA <sebassga34@gmail.com>
2025-01-16 00:16:36 -0500

    feat: register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDto } from './dtos';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -8,5 +10,39 @@ export class AuthService extends PrismaClient implements OnModuleInit {
   onModuleInit() {
     this.$connect();
     this.logger.log('Auth-ms db Mongodb connected');
+  }
+
+  async registerUser({ email, name, password }: RegisterUserDto) {
+    try {
+      const userDb = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (userDb)
+        throw new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: 'User already exists',
+        });
+
+      const user = await this.user.create({
+        data: {
+          email,
+          password,
+          name,
+        },
+      });
+
+      return {
+        user,
+        token: 'abs',
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: error.message,
+      });
+    }
   }
 }
